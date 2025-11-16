957aaee09c9188e8d7bba2eaacb709a8de64cf47
Muhammad Ali <malitariq5324@gmail.com>
2025-11-16 06:55:55 +0500

    refactor: Renamed variables and change while condition
    - Renamed selected_operation to selectedOperation.
    
    - Directly takin input in the continue_status variable.
    
    - Formatted functions.

--- console-chaining-calculator/main.py
+++ console-chaining-calculator/main.py
@@ -1,26 +1,18 @@
-def add(n1, n2):
-    return n1 + n2
+def add(n1, n2):return n1 + n2
 
+def subtract(n1, n2):return n1 - n2
 
-def subtract(n1, n2):
-    return n1 - n2
+def multiply(n1, n2):return n1 * n2
 
+def divide(n1, n2): return n1 / n2
 
-def multiply(n1, n2):
-    return n1 * n2
-
-
-def divide(n1, n2):
-    return n1 / n2
-
-
-def calculations(answer_1):
+def calculations(prevAnswer):
     for _ in operations:
         print(f"\n{_}")
-    selected_operation = input("\nSelect an Operation from the above:")
+    selectedOperation = input("\nSelect an Operation from the above:")
     num3 = float(input("\nEnter next number = "))
-    answer_2 = operations[selected_operation](answer_1, num3)
-    print(f"\n{answer_1} {selected_operation} {num3} = {answer_2}")
+    answer_2 = operations[selectedOperation](prevAnswer, num3)
+    print(f"\n{prevAnswer} {selectedOperation} {num3} = {answer_2}")
 
 
 operations = {
@@ -34,16 +26,15 @@ num1 = float(input("\nEnter first number = "))
 for key, value in operations.items():
     print(f"\nKey:{key}, Value:{value}, Type:{type(value)}")
 
-selected_operation = input("\nSelect an Operation from the above:")
+selectedOperation = input("\nSelect an Operation from the above:")
 num2 = float(input("\nEnter second number = "))
 
-answer_1 = operations[selected_operation](num1, num2)
+answer_1 = operations[selectedOperation](num1, num2)
 
-print(f"\n{num1} {selected_operation} {num2} = {answer_1}")
+print(f"\n{num1} {selectedOperation} {num2} = {answer_1}")
 
-while 2 > 0:
-    print(f"\nType 'yes' if you want to continue calculation with {answer_1} or type 'no' to exit: ")
-    continue_status = input("")
+while True:
+    continue_status = input(f"\nType 'yes' if you want to continue calculation with {answer_1} or type 'no' to exit: ")
     if continue_status == "no":
         break
     elif continue_status == "yes":
