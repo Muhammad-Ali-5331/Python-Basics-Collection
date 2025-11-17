e1716359c76b3ae03fe483801a41d6311307aa6b
Muhammad Ali <malitariq5324@gmail.com>
2025-11-17 16:45:24 +0500

    feat: Add lifespan remaining calculator
    Implements a script that takes the user's current age and calculates the time remaining until they reach 90 years old, reporting the result in years, days, weeks, and months.

---
+++ lifespan-remaining-calculator/main.py
@@ -0,0 +1,6 @@
+age = int(input("Enter Your Age in Years = "))
+print(f"You have {90 - age} years, {90*365 - age*365} days, {90*52 - age*52} weeks and {90*12 - age*12} months left.")
+
+#Method 2
+#message = f"You have {90 - age} years, {90*365 - age*365} days, {90*52 - age*52} weeks and {90*12 - age*12} months left."
+#print(message)
